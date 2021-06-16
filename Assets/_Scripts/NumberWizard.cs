10cd55b7bfe6927da6d5e217885b97c435442e24
lemonzninja <budninja36@gmail.com>
2021-06-15 22:07:52 -0400

    0
    0-

---
+++ Assets/_Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using TMPro;
+using UnityEngine;
+
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] private int max;
+    [SerializeField] private int min;
+    [SerializeField] private TextMeshProUGUI guessText;
+
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
